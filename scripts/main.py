9e3a68af89984a0a5447dcf2ea9f3c3ff317bb86
swanev <68728245+swanev@users.noreply.github.com>
2021-03-19 12:30:04 +0700

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -4,7 +4,10 @@ print("PRINT FROM PYTHON: ACTION 1.2 - STARTED")
 
 timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
+tag=open('tag.txt', 'r')
+tag=tag.read()
+
 with open('log.md', 'w') as f:
-    f.write(f'# {timestamp}')
+    f.write(f'# {timestamp, tag}')
     
 print("PRINT FROM PYTHON: ACTION 1.2 - COMPLETED")
